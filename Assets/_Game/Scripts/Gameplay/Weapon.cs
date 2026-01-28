a7970700af5f3b329b9ff2f6fc64ecc3c9cce7a4
ktokto313 <kto3132006@gmail.com>
2026-01-28 09:35:29 +0700

    Make Weapon class

---
+++ Assets/_Game/Scripts/Gameplay/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Game.Scripts.Gameplay
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Weapon : MonoBehaviour
+    {
+        private WeaponType _weaponType;
+        private WeaponData _weaponData;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Entity entityHit = (Entity)(other.gameObject);
+            entityHit.LowerHealth(_weaponData.getDamageOnWolf());
+        }
+    }
+
+    public enum WeaponType
+    {
+        Ranged,
+        Melee
+    }
+}
---
+++ Assets/_Game/Scripts/Gameplay/WeaponData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts.Gameplay
+{
+    [CreateAssetMenu(fileName = "weaponExample", menuName = "_Game/Scripts/Gameplay/weaponExample")]
+    public class WeaponData : ScriptableObject
+    {
+        [SerializeField]
+        private int damageOnWolf = 1;
+
+        public int getDamageOnWolf()
+        {
+            return damageOnWolf;
+        }
+    }
+}
