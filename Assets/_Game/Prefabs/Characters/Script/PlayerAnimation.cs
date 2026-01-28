07a8ff325d828ea31728e480ccb416d9c0376c31
minhcamon <nhatminhyt06@gmail.com>
2026-01-29 02:00:08 +0700

    Create Player
    - Duck
    - Punch (L/R)
    - Jump

---
+++ Assets/_Game/Prefabs/Characters/Script/PlayerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private PlayerController player;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            player.OnAttackAction += PlayAttack;
+            player.OnJumpAction += PlayJump;
+        }
+    }
+
+    private void Update()
+    {
+        if (player)
+        {
+            anim.SetBool("IsDucking", player.IsDucking);
+            anim.SetBool("IsOnAir",  player.IsOnAir);
+        }
+    }
+    
+    
+    // PLAY METHOD
+
+    private void PlayAttack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    private void PlayJump()
+    {
+        anim.SetTrigger("Jump");
+    }
+    
+}
