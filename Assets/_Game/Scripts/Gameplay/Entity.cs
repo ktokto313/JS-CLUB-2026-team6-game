30e0ebc9d5f6fe592632ec7371b8e2473c76983f
ktokto313 <kto3132006@gmail.com>
2026-01-28 09:34:46 +0700

    Fix Entity class to match flow

--- Assets/_Game/Scripts/Gameplay/Entity.cs
+++ Assets/_Game/Scripts/Gameplay/Entity.cs
@@ -1,28 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using _Game.Scripts.Gameplay;
 using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
-    private int Health { set; get;  }
-    private Weapon Weapon { set; }
+    private int _health;
+    private Weapon _weapon;
 
     public Action OnDeathAction;
     public Action OnHitAction;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        Health = 0;
+        _health = 0;
+    }
+    
+    public void SetHealth(int health)
+    {
+        if (health < this._health) OnHit();
+        CheckHealth();
+        this._health = health;
     }
 
-    protected void onHit() 
+    public void LowerHealth(int amount)
+    {
+        OnHit();
+        CheckHealth();
+        this._health -= amount;
+    }
+
+    private void CheckHealth()
+    {
+        if (this._health <= 0) OnDeathAction();
+    }
+
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    public void SetWeapon(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+
+    public Weapon GetWeapon()
+    {
+        return _weapon;
+    }
+
+    protected void OnHit() 
     {
         OnHitAction?.Invoke();
     }
     
-    protected void onDeath() 
+    protected void OnDeath() 
     {
         OnDeathAction?.Invoke();
     }
