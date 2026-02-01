ef082baec6bc59c07ab5f7647f2882c4410759dd
ktokto313 <kto3132006@gmail.com>
2026-02-01 23:19:19 +0700

    Refactor Entity and Weapon (#3)

--- Assets/_Game/Scripts/Gameplay/Entity.cs
+++ Assets/_Game/Scripts/Gameplay/Entity.cs
@@ -1,28 +1,52 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using _Game.Scripts.Gameplay;
 using UnityEngine;
 
-public abstract class Entity : MonoBehaviour
+public class Entity : MonoBehaviour
 {
-    private int Health { set; get;  }
-    private Weapon Weapon { set; }
+    private int _health;
+    [SerializeField]
+    private Projectile projectile;
 
     public Action OnDeathAction;
     public Action OnHitAction;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        Health = 0;
+        _health = 0;
+    }
+    
+    public void SetHealth(int health)
+    {
+        if (health < this._health) OnHit();
+        CheckHealth();
+        this._health = health;
     }
 
-    protected void onHit() 
+    public void LowerHealth(int amount)
+    {
+        OnHit();
+        this._health -= amount;
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        if (this._health <= 0) OnDeathAction();
+    }
+
+    public int GetHealth()
+    {
+        return _health;
+    }
+
+    protected void OnHit() 
     {
         OnHitAction?.Invoke();
     }
     
-    protected void onDeath() 
+    protected void OnDeath() 
     {
         OnDeathAction?.Invoke();
     }
---
+++ Assets/_Game/Scripts/Gameplay/Projectile.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace _Game.Scripts.Gameplay
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class Projectile : ScriptableObject
+    {
+        [SerializeField] private int damage = 0;
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Entity entityHit = (other.gameObject.GetComponent<Entity>());
+            entityHit?.LowerHealth(damage);
+        }
+    }
+}
