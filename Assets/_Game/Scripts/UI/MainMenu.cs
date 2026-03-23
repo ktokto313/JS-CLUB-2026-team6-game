02f82b2b5f11a20d5bab76c71ae5e0823c4b2c0b
Nam Anh <nnamanh363@gmail.com>
2026-03-23 22:02:34 +0700

    em khong biet dau

--- Assets/_Game/Scripts/UI/MainMenu.cs
+++ Assets/_Game/Scripts/UI/MainMenu.cs
@@ -1,44 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 using UnityEngine.SceneManagement;
-using _Game.Scripts.Core;
+using _Game.Scripts.Core; 
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject musicSlider;
-    public GameObject sfxSlider;
+
+    public Slider musicSlider; 
+    public Slider sfxSlider;   
+    public AudioSource musicSource; 
+    public AudioSource sfxSource;
+
+    void Start()
+    {
+    
+        if (musicSlider != null)
+        {
+            
+            if (musicSource != null) musicSlider.value = musicSource.volume;
+            
+            musicSlider.onValueChanged.AddListener(OnMusicSliderChanged);
+        }
+
+        if (sfxSlider != null)
+        {
+            if (sfxSource != null) sfxSlider.value = sfxSource.volume;
+            
+            sfxSlider.onValueChanged.AddListener(OnSFXSliderChanged);
+        }
+    }
 
     public void StartGame() {
+        Time.timeScale = 1f; 
         SceneManager.LoadScene("testchotien"); 
+    }
+
+    public void RestartGame() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
     public void ToggleMusic() {
-        if(musicSlider != null) musicSlider.SetActive(!musicSlider.activeSelf);
+        if(musicSlider != null) musicSlider.gameObject.SetActive(!musicSlider.gameObject.activeSelf);
     }
-    
 
     public void ToggleSFX() {
-        if(sfxSlider != null) sfxSlider.SetActive(!sfxSlider.activeSelf);
+        if(sfxSlider != null) sfxSlider.gameObject.SetActive(!sfxSlider.gameObject.activeSelf);
     }
+
     
     public void OnMusicSliderChanged(float value)
     {
+       
+        if (musicSource != null) 
+        {
+            musicSource.volume = value;
+        }
         
         if (EventManager.current != null)
         {
             EventManager.current.onMusicVolumeSliderUpdate(value);
         }
     }
-    
-    
+
     public void OnSFXSliderChanged(float value)
     {
+        
+        if (sfxSource != null) 
+        {
+            sfxSource.volume = value;
+        }
 
         if (EventManager.current != null)
         {
             EventManager.current.onSFXVolumeSliderUpdate(value);
         }
     }
-    
 }
