5b3f6e86289b6eb3f546dc203bdac07184e7cb37
ktokto313 <kto3132006@gmail.com>
2026-03-26 19:15:35 +0700

    Change to bg lerp time

--- Assets/_Game/Scripts/Core/BackgroundController.cs
+++ Assets/_Game/Scripts/Core/BackgroundController.cs
@@ -9,6 +9,7 @@ public class BackgroundController : MonoBehaviour
     public static BackgroundController Instance { private set; get; }
     
     [SerializeField] private List<GameObject> backgroundList;
+    [SerializeField] private float transitionTime = 2f;
     [SerializeField] private int waveCount = 5;
     private int currentWave = 0;
     private int currentBackgroundIndex = 0;
@@ -25,8 +26,6 @@ public class BackgroundController : MonoBehaviour
     public void OnNextWave()
     {
         currentWave++;
-        Debug.LogError("wave count " + waveCount);
-        Debug.LogError((waveCount / backgroundList.Count) * (currentBackgroundIndex + 1) + " " + currentWave);
         if ((waveCount / backgroundList.Count) * (currentBackgroundIndex + 1) >= currentWave) return;
         if (currentBackgroundIndex >= backgroundList.Count - 1) return; // Out of backgrounds.
 
@@ -67,7 +66,7 @@ public class BackgroundController : MonoBehaviour
             Vector3 nextStart = nextTr.position;
             Vector3 delta = new Vector3(30f, 0f, 0f);
 
-            float duration = 1f;
+            float duration = transitionTime;
             float elapsed = 0f;
 
             queuedTransitions--; // Consume 1 transition.
